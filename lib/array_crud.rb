8fe4bd6e922108b43542bc7a1eaffbba11890c33
DAMETRES PERRY <dametres.perry21@gmail.com>
2020-06-24 03:15:43 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,34 +3,32 @@ def create_an_empty_array
 end
 
 def create_an_array 
-  evelution = ["Dogs", "Cats", "Wolf", "Lion"]
+  ["Dogs", "Cats", "Wolf", "Lion"]
   
 end
 
 def add_element_to_end_of_array(array, element)
-  evelution01 = ["Dogs", "Cats"]
-  evelution << "Worlf"
+ 
 end
 
 def add_element_to_start_of_array(array, element) 
-  evelution12 = ["Cats", "Worlf"]
-  evelution12.unshift("Dogs")
+  
 end
 
 def remove_element_from_end_of_array(array)
-  evelution.pop
+  array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  create_an_array.shift
+  array.shift
 end
 
 def retrieve_element_from_index(array, index_number) =
-  
+  array[3]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  array
 end
 
 def retrieve_last_element_from_array(array)
