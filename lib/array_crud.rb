c438dd110f171d2c774f37687e91c0e7a400484e
DAMETRES PERRY <dametres.perry21@gmail.com>
2020-06-24 03:18:20 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
- 
+ array << "Turtle"
 end
 
 def add_element_to_start_of_array(array, element) 
@@ -28,9 +28,9 @@ def retrieve_element_from_index(array, index_number) =
 end
 
 def retrieve_first_element_from_array(array)
-  array
+  array.first
 end
 
 def retrieve_last_element_from_array(array)
-  
+  array.last
 end
